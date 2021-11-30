54d698ff166a3858286c42e5f2cf32a16143e319
Jayakrishnan <Jayakrishnan1012@gmail.com>
2021-11-30 21:16:31 +0530

    Added Heap

--- Heap/Heap.js
+++ Heap/Heap.js
@@ -1,61 +1,75 @@
-import Comparator from "./Comparator.js";
+class MaxHeap {
+  constructor() {
+    this.heap = [];
+  }
 
-class Heap {
-  constructor(comparatorFunction) {
-    if (new.target === Heap) {
-      throw new TypeError("Cannot create Heap Instance directly");
+  size() {
+    return this.heap.length;
+  }
+  empty() {
+    return this.size() === 0;
+  }
+
+  heapify() {
+    let index = this.size() - 1;
+
+    while (index > 0) {
+      const element = this.heap[index];
+      const parentIndex = Math.floor((index - 1) / 2);
+      const parent = this.heap[parentIndex];
+
+      if (parent[0] > element[0]) break;
+      this.heap[index] = parent;
+      this.heap[parentIndex] = element;
+      index = parentIndex;
     }
-
-    this.heapContainer = [];
-    this.compare = new Comparator(comparatorFunction);
+  }
+  insert(value) {
+    this.heap.push(value);
+    this.heapify();
   }
 
-  getLeftChildIndex(parentIndex) {
-    return 2 * parentIndex + 1;
-  }
+  sinkDown(index) {
+    const left = 2 * index + 1;
+    const right = 2 * index + 2;
+    let largest = index;
+    const length = this.size();
 
-  getRightChildIndex(parentIndex) {
-    return 2 * parentIndex + 2;
-  }
-
-  getParentIndex(childIndex) {
-    return Math.floor((childIndex - 1) / 2);
-  }
-
-  hasParent(childIndex) {
-    return this.getParentIndex(childIndex) >= 0;
-  }
-
-  hasLeftChild(parentIndex) {
-    return this.getLeftChildIndex(parentIndex) < this.heapContainer.length;
-  }
-
-  hasRightChild(parentIndex) {
-    return this.getRightChildIndex(parentIndex) < this.heapContainer.length;
-  }
-
-  leftChild(parentIndex) {
-    return this.heapContainer[this.getLeftChildIndex(parentIndex)];
-  }
-  rightChild(parentIndex) {
-    return this.heapContainer[this.getRightChildIndex(parentIndex)];
-  }
-
-  parent(childIndex) {
-    return this.heapContainer[this.getParentIndex(childIndex)];
-  }
-
-  swap(index1, index2) {
-    const tmp = this.heapContainer[index2];
-    this.heapContainer[index2] = this.heapContainer[index1];
-    this.heapContainer[index1] = tmp;
-  }
-
-  peek() {
-    if (this.heapContainer.length === 0) {
-      return null;
+    if (left < length && this.heap[left][0] > this.heap[largest][0]) {
+      largest = left;
     }
+    if (right < length && this.heap[right][0] > this.heap[largest][0]) {
+      largest = right;
+    }
+    if (largest !== index) {
+      const tmp = this.heap[largest];
+      this.heap[largest] = this.heap[index];
+      this.heap[index] = tmp;
 
-    return this.heapContainer[0];
+      this.sinkDown(largest);
+    }
+  }
+
+  extractMax() {
+    const max = this.heap[0];
+    const tmp = this.heap.pop();
+    if (!this.empty()) {
+      this.heap[0] = tmp;
+      this.sinkDown(0);
+    }
+    return max;
   }
 }
+
+const maxHeap = new MaxHeap();
+maxHeap.insert([4]);
+maxHeap.insert([3]);
+maxHeap.insert([6]);
+maxHeap.insert([111]);
+maxHeap.insert([9]);
+maxHeap.insert([2]);
+
+console.log(maxHeap);
+const mx = maxHeap.extractMax();
+
+console.log("Extracted: ", mx);
